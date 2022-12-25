ed64c60ae3f4c0c80b6e59034406b55bc61ea063
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-12-25 17:35:52 -0300

    feat: Added post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,53 @@
+import styles from "./Post.module.css";
+
 export function Post() {
-    return <div>Post</div>;
+    return (
+        <div className={styles.post}>
+            <article>
+                <header>
+                    <div className={styles.author}>
+                        <img
+                            className={styles.avatar}
+                            src="https://github.com/lucasmbrute2.png"
+                            alt="Profile photo"
+                        />
+                        <div className={styles.authorInfo}>
+                            <strong>Lucas Dantas</strong>
+                            <span>Web Developer</span>
+                        </div>
+                    </div>
+
+                    <time
+                        title="11 de Maio as 8:13h"
+                        dateTime="2022-05-11 08:13:30"
+                    >
+                        Publicado há 1h
+                    </time>
+                </header>
+
+                <div className={styles.content}>
+                    <p>
+                        <p>Fala galeraa 👋</p>
+
+                        <p>
+                            Acabei de subir mais um projeto no meu portifa. É um
+                            projeto que fiz no NLW Return, evento da Rocketseat.
+                            O nome do projeto é DoctorCare 🚀
+                        </p>
+
+                        <p>
+                            <a href="#">jane.design/doctorcare</a>
+                        </p>
+
+                        <p>
+                            <a href="#">
+                                <a href="#">#novoprojeto</a>{" "}
+                                <a href="#">#nlw</a> <a href="#">#rocketseat</a>
+                            </a>
+                        </p>
+                    </p>
+                </div>
+            </article>
+        </div>
+    );
 }
